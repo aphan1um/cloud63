e3bda7c1cc128ea21ed391745c8a628565c61453
aphan1 <aphan1@student.unimelb.edu.au>
2019-05-05 17:14:41 +1000

    Update simple Twitter script

--- twitterContainer/twitterScript.py
+++ twitterContainer/twitterScript.py
@@ -1,38 +1,17 @@
+# NOTE: Requires tweepy and couchdb packages (use pip to install)
+
 import tweepy
-from cloudant.client import CouchDB
-import sys
-import ast
+import couchdb
 
-# Using: https://boundingbox.klokantech.com/
-# With: https://developer.twitter.com/en/docs/tweets/filter-realtime/guides/basic-stream-parameters.html
-bounding_box = [112.51, -44.19, 154.34, -9.8] # [72.2,-55.3,168.2,-9.1]
-
-# The four keys needed for twitter bot
-# TODO: Credit to https://stackoverflow.com/a/22889470
-# TODO: Harvest tweets from followers instead of real-time (??)
-print(sys.argv[2])
-print(sys.argv[1])
-keys = ast.literal_eval(sys.argv[2])
-
-# STEP: Connect to Twitter REST API services
-auth = tweepy.OAuthHandler(keys["consumer_key"], keys["consumer_secret"])
-auth.set_access_token(keys["access_key"], keys["access_secret"])
-api = tweepy.API(auth)
-
-# STEP: Connect to CouchDB (very prototypey)
-# Help from: https://python-cloudant.readthedocs.io/en/latest/getting_started.html
-db_url = sys.argv[1]
-client = CouchDB("admin", "admin", url = db_url, connect = True)
-tweetDB = client.create_database(keys["name"].lower())
-
-if not tweetDB.exists():
-    raise Exception("ERROR: Database %s was not created!" % keys["name"].lower())
-
-# TODO: Using http://docs.tweepy.org/en/latest/streaming_how_to.html
 class SimpleStreamListener(tweepy.StreamListener):
     def on_status(self, status):
-        status._json['_id'] = status._json['id_str']
-        tweetDB.create_document(status._json)
+        try:
+            status._json['_id'] = status._json['id_str']
+            tweetDB.save(status._json)
+        except couchdb.http.ResourceConflict as e:
+            print("Tweet already added: %s" % status._json['_id'])
+        else:
+            print("Tweet accepted: %s" % status._json['_id'])
 
     def on_error(self, status_code):
         # TODO: Error handling
@@ -40,11 +19,61 @@ class SimpleStreamListener(tweepy.StreamListener):
             # returning False in on_data disconnects the stream
             return False
 
+def get_auth():
+    auth = tweepy.OAuthHandler('2znfHb14GLJ2x3RfoftrkQoCU', 'mT8zKjHNrsQhUE2nKd5Ob4uKppWgIZFFUlTylr1VlTqTFv02WK')
+    auth.set_access_token('1121009594254249986-CuuaCvTUYqjKLS2uBROkk1cQMLDI30', 'bqwPb7pyqYiKbMA0qhXrfvc5cKJq7WXI6wsvBCQ0Hay7Y')
+    return tweepy.API(auth)
 
-########### MAIN CODE HERE: ############
-myStreamListener = SimpleStreamListener()
+def limit_handled(cursor):
+    while True:
+        try:
+            yield cursor.next()
+        except tweepy.RateLimitError:
+            print("[WARNING] Rate limit hit! 15 min wait...")
+            time.sleep(15 * 60)
 
-myStream = tweepy.Stream(auth = api.auth, listener = myStreamListener)
+def start_realtime():
+    # Using: https://boundingbox.klokantech.com/
+    # With: https://developer.twitter.com/en/docs/tweets/filter-realtime/guides/basic-stream-parameters.html
+    bounding_box = [112.51, -44.19, 154.34, -9.8] # [72.2,-55.3,168.2,-9.1]
 
-# this starts the real-time listener
-myStream.filter(locations = bounding_box, is_async = True)
+    myStreamListener = SimpleStreamListener()
+    myStream = tweepy.Stream(auth = api.auth, listener = myStreamListener)
+    # this starts the real-time listener
+    myStream.filter(locations = bounding_box, is_async = True)
+
+api = get_auth()
+
+db_url = "http://user:pass@127.0.0.1:5700/"
+server = couchdb.Server(db_url)
+tweetDB = server["twit"]
+
+
+#########################    MAIN CODE HERE    #########################
+
+query = 'soda OR butter OR "potato chips" OR "coca cola" OR cookie OR pastry OR unhealthy OR cake OR sugary'
+geoc = "-36.565842,145.043926,442km"
+total_tweets_counted = 0
+total_tweets_accepted = 0
+
+print("Starting script...")
+
+for tweet in limit_handled(tweepy.Cursor(api.search, q=query, lang='en', geocode=geoc, tweet_mode='extended').items()):
+    doc = tweet
+
+    if tweet.place is not None or tweet.geo is not None or tweet.coordinates is not None:
+        doc._json['_id'] = doc._json['id_str']
+        total_tweets_counted += 1
+
+        try:
+            tweetDB.save(doc._json)
+        except couchdb.http.ResourceConflict as e:
+            print("Tweet already added: %s" % doc._json['_id'])
+        else:
+            print("Tweet accepted: %s" % doc._json['_id'])
+            total_tweets_accepted += 1
+    else:
+        print("Skipped tweet: %s" % doc._json['id'])
+
+
+print("Ending script... Counted: %d\tAccepted: %d" % (total_tweets_counted, total_tweets_accepted))
